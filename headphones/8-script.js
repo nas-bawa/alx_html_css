ced629ec9251902fed8052a867ce4171e2912d1c
Nasiru Bawa <haskenasir8@gmail.com>
2025-12-07 14:36:55 +0000

    Added a humburger menu to the navigation with JS

---
+++ headphones/8-script.js
@@ -0,0 +1,9 @@
+<script>
+  const hamburger = document.querySelector('.hamburger');
+  const navLinks = document.querySelector('.nav-links');
+
+  hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    hamburger.classList.toggle('open');
+  });
+</script>
